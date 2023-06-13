65ed1cf957f7a19767d8bce9bc6fe74de5ca77fd
Charan-05 <cmokara@gitam.in>
2023-06-13 21:47:04 +0530

    no changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
 
-function App() {
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [calculation, setCalculation] = useState(0);
+  useEffect(() => {
+    setCalculation(() => count * 2);
+  }, [count]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Count of User Taps: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>Click here to Count</button>
+      <p>Calculation: {calculation}</p>
+    </>
   );
 }
 
-export default App;
+export default Counter;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Counter />);
